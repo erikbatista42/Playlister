98503bddb73ed37526365fe00b39124647545d5b
Erik Batista <erikbatista42@gmail.com>
2019-09-29 15:52:28 -0700

    user can now create playlists

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 
@@ -18,6 +18,21 @@ def playlists_index():
     # show all playlists
     return render_template("playlists_index.html", playlists=playlists.find())
 
+@app.route("/playlists/new")
+def playlist_new():
+    return render_template("playlists_new.html")
+
+@app.route("/playlists", methods=["POST"])
+def playlist_submit():
+    # submit a new playlist 
+    print(request.form.to_dict())
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    playlists.insert_one(playlist)
+    return redirect(url_for("playlists_index"))
 
 if __name__ == '__main__':
     app.run(debug=True)

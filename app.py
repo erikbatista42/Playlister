5f6da80206a8571413888e25e706f6551c8b692f
Erik Batista <erikbatista42@gmail.com>
2019-09-29 15:10:40 -0700

    Users can see playlists

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 
+
+# mock array of objects
+playlists = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+]
+
 app = Flask(__name__)
 @app.route("/")
-def index():
-    # return to homepage
-    return render_template("home.html", msg="flask is ok")
+def playlists_index():
+    # show all playlists
+    return render_template("playlists_index.html", playlists=playlists)
+
 
 if __name__ == '__main__':
     app.run(debug=True)

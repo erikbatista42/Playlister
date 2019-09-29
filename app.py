b26dca9be9457b71c7787ea8cf0d0a1867e69975
Erik Batista <erikbatista42@gmail.com>
2019-09-29 15:28:11 -0700

    added MonogoDB & playlist model

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
 
 
-# mock array of objects
-playlists = [
-    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-]
+client = MongoClient()
+db = client.Playlister
+playlists = db.playlists
+    # mock array of objects
+# playlists = [
+#     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+#     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' },
+#     {'title': 'Favorite Music', 'description': ["Better not", "No fun"]}
+# ]
 
 app = Flask(__name__)
 @app.route("/")
 def playlists_index():
     # show all playlists
-    return render_template("playlists_index.html", playlists=playlists)
+    return render_template("playlists_index.html", playlists=playlists.find())
 
 
 if __name__ == '__main__':
